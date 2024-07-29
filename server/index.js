7d3413f81b4ccff7d1cb4e7dfa1f3f438e6a6ee9
raamprasaadh <ramprasadhsurya@gmail.com>
2024-07-29 14:28:54 +0530

    adds eslint to the server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-undef
 const express = require('express');
 
 const app = express();
@@ -5,5 +6,5 @@ const app = express();
 const PORT = 4000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+console.log(`Server is running on port ${PORT}`);
 });
