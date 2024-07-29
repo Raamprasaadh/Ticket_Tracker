90b30bf452ff96de5c892b9ce4a71f71837db3a4
raamprasaadh <ramprasadhsurya@gmail.com>
2024-07-29 15:01:47 +0530

    adds db connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
-// eslint-disable-next-line no-undef
+/* eslint-disable no-undef */
 const express = require('express');
-
+const mongoose = require('mongoose');
+require("dotenv").config();
+const {  MONGO_URL, DB_NAME } = process.env;
+const PORT = 4000;
 const app = express();
 
-const PORT = 4000;
+mongoose.connect(MONGO_URL, {
+    dbName: DB_NAME,
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+}).then(() => console.log("MongoDB is connected successfully"))
+.catch((err) => console.error(err));
 
 app.listen(PORT, () => {
 console.log(`Server is running on port ${PORT}`);
 });
+
+app.get('/',(req,res)=>{
+    res.send('respons from server');
+})
